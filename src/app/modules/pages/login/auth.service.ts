752c02203e454525d5a00baff7745e4eccace3fc
MisoleM <marjanovic.ra172.2019@uns.ac.rs>
2023-04-01 12:47:58 +0200

    Authentication complete

---
+++ src/app/modules/pages/login/auth.service.ts
@@ -0,0 +1,33 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Login } from '../../model/login';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  apiHost: string = 'http://localhost:8084';
+  headers: HttpHeaders = new HttpHeaders({ 'Content-Type': 'application/json' });
+
+constructor(private http: HttpClient) { }
+
+login(login:Login): Observable<any> {
+  return this.http.post<Login>(this.apiHost + '/auth/login', login);
+}
+
+isAuthorized(allowedRoles: string[]): boolean {
+  // check if the list of allowed roles is empty, if empty, authorize the user to access the page
+  if (allowedRoles == null || allowedRoles.length === 0) {
+    return true;
+  }
+
+  // get token from local storage or state management
+ const role = localStorage.getItem('role');
+
+// check if the user roles is in the list of allowed roles, return true if allowed and false if not allowed
+  return allowedRoles.includes(role!);
+}
+
+}
